044cccb72394dcb97d72c3a4a403f2658349c6be
Ryan Wise <ryan@ryanwise.me>
2018-10-12 10:33:00 -0400

    Started working on controlling through WS interface

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
-import {window, StatusBarAlignment, StatusBarItem, ExtensionContext} from 'vscode';
+import {window, StatusBarAlignment, StatusBarItem, ExtensionContext, commands} from 'vscode';
 import {readFileSync} from 'fs';
+const WebSocket = require('ws');
 //Custom imports
 import GPMDPData from './interfaces/gpmdpdata';
 
@@ -7,13 +8,25 @@ import GPMDPData from './interfaces/gpmdpdata';
 export function activate(context: ExtensionContext) {
     let mc = new MusicController();
     setInterval(mc.updateStatusBar.bind(mc), 1000);
+
+    commands.registerCommand('player.pair', () => {
+        console.log("pairing....")
+        mc.pair();
+    });
+
 }
 
 class MusicController {
     private _statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left, 10);
+    private _connectionString: string = "ws://localhost:5672";
+    private _connected: boolean = false;
+    private _MAX_ATTEMPTS: number = 3;
+
     private _songData?: GPMDPData;
+    private _ws: any;
 
     public constructor() {
+        this.connect();
         this.updateStatusBar();
         this._statusBarItem.show();
     }
@@ -32,6 +45,39 @@ class MusicController {
         let progress = " ["+currentTime+" / "+totaltime+"]";
 
         this._statusBarItem.text = playPause + " " + songtitle + " " + progress;
+    }
+
+    public pair() {
+        const connectionObject = {
+            namespace: 'connect',
+            method: 'connect',
+            arguments: ["Visual Studio: Code"]
+        };
+        this.sendData(connectionObject);
+    }
+
+    private connect() {
+        this._ws = new WebSocket(this._connectionString);
+        this._ws.on('open', () => this._connected = true);
+        this._ws.on('close', () => this._connected = false);
+
+        this._ws.on('message', (data: any) => console.log(data));
+    }
+
+    private sendData(data: object, attempts: number = 0) {
+        console.log(data, this._connected, attempts);
+        if (!this._connected) {
+            this.connect();
+            attempts++
+
+            if (attempts <= this._MAX_ATTEMPTS)
+                setTimeout(() => this.sendData(data, attempts), 500);
+            else
+                window.showErrorMessage('Error could not connect to player');
+            return;
+        }
+
+        this._ws.send(JSON.stringify(data));
     }
 
     // Read JSON file store don system
